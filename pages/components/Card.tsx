b08a88199b49aee67346101997d9e0c53fb0ef82
Anton Eriksson <anton.eriksson@umu.se>
2021-09-28 14:19:06 +0200

    refactor: Redesign card

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,41 +1,42 @@
-import { Box, Text, Tooltip, Image } from '@chakra-ui/react';
+import { Box, Text, Tooltip } from '@chakra-ui/react';
 
 export function Card({ title, description, src, isActive, onClick, ...props }: any) {
     return (
-        <Box
-            p={2}
-            boxShadow={isActive ? "outline" : "base"}
-            rounded="md"
-            bg="white"
-            maxWidth={200}
-            _hover={{ boxShadow: "outline" }}
-            onClick={onClick}
-            style={{ cursor: "pointer" }}
-            {...props}
+        <Tooltip
+            hasArrow
+            fontSize="sm"
+            shadow="dark-lg"
+            label={description}
+            placement="bottom"
+            bg="gray.50"
+            color="gray.600"
         >
-            <Text
-                fontSize="sm"
-                color={isActive ? "gray.900" : "gray.700"}
-                fontWeight={isActive ? "600" : "normal"}
+            <Box
+                p={3}
+                boxShadow={isActive ? "outline" : "base"}
+                rounded="md"
+                background={`linear-gradient(transparent 67%, white 72%), url("${src}") -15px -15px no-repeat`}
+                bgSize="120%"
+                pos="relative"
+                width={140}
+                height={155}
+                _hover={{ boxShadow: "outline" }}
+                onClick={onClick}
+                style={{ cursor: "pointer" }}
+                {...props}
             >
-                {title}
-            </Text>
-            <Tooltip
-                hasArrow
-                fontSize="sm"
-                shadow="dark-lg"
-                label={description}
-                placement="bottom"
-                bg="gray.50"
-                color="gray.600"
-            >
-                <Image
-                    rounded="md"
-                    htmlWidth={200}
-                    htmlHeight={200}
-                    src={src}
-                />
-            </Tooltip>
-        </Box>
+                <Text
+                    pos="absolute"
+                    bottom={7}
+                    display="block"
+                    height={3}
+                    fontSize={12}
+                    color={isActive ? "gray.900" : "gray.700"}
+                    fontWeight={isActive ? "600" : "normal"}
+                >
+                    {title}
+                </Text>
+            </Box>
+        </Tooltip>
     )
 }
