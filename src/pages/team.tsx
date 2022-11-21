519b18d9c5edc1785fb11f1bd8aa2eb7da0a51b3
Daniel Edler <edler.dev@gmail.com>
2022-11-21 22:38:13 +0100

    Add email links

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -34,9 +34,32 @@ const WebsiteIcon = ({ size }: { size: number }) => (
   </svg>
 );
 
+const EmailIcon = ({ size }: { size: number }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 64 64"
+    width={size}
+    height={size}
+    style={{ display: "inline-block" }}
+  >
+    <path
+      id="Mail"
+      d="M58.0034485,8H5.9965506c-3.3136795,0-5.9999995,2.6862001-5.9999995,6v36c0,3.3137016,2.6863203,6,5.9999995,6
+      h52.006897c3.3137016,0,6-2.6862984,6-6V14C64.0034485,10.6862001,61.3171501,8,58.0034485,8z M62.0034485,49.1108017
+      L43.084549,30.1919994l18.9188995-12.0555992V49.1108017z M5.9965506,10h52.006897c2.2056007,0,4,1.7943001,4,4v1.7664003
+      L34.4677505,33.3134003c-1.4902,0.9492989-3.3935013,0.9199982-4.8495998-0.0703011L1.9965508,14.4694996V14
+      C1.9965508,11.7943001,3.7910507,10,5.9965506,10z M1.9965508,16.8852005L21.182251,29.9251003L1.9965508,49.1108017V16.8852005z
+      M58.0034485,54H5.9965506c-1.6473999,0-3.0638998-1.0021019-3.6760998-2.4278984l20.5199013-20.5200024l5.6547985,3.843401
+      c1.0859013,0.7383003,2.3418007,1.1083984,3.5995998,1.1083984c1.1953011,0,2.3925018-0.3339996,3.4463005-1.0048981
+      l5.8423996-3.7230015l20.2961006,20.2961025C61.0673485,52.9978981,59.6508713,54,58.0034485,54z"
+    />
+  </svg>
+);
+
 type Person = {
   name: string;
   image: string;
+  email: string;
   webpage: string;
   scholar?: string;
   bio: string;
@@ -51,6 +74,7 @@ export function getStaticProps(): { props: { team: Person[] } } {
         {
           name: "Daniele Silvestro",
           image: "/images/team/DS.png",
+          email: "daniele@captaintech.ai",
           webpage: "https://www.unifr.ch/bio/en/groups/silvestro",
           scholar:
             "https://scholar.google.com/citations?hl=en&user=X1jlzMoAAAAJ&view_op=list_works&sortby=pubdate",
@@ -64,6 +88,7 @@ export function getStaticProps(): { props: { team: Person[] } } {
         {
           name: "Stefano Goria",
           image: "/images/team/SG.png",
+          email: "stefano@captaintech.ai",
           webpage: "https://thymia.ai/",
           bio: `Stefano Goria has a PhD in theoretical physics and is the CTO and
             co-founder of Thymia Limited, a London-based mental health tech
@@ -72,6 +97,7 @@ export function getStaticProps(): { props: { team: Person[] } } {
         {
           name: "Thomas Sterner",
           image: "/images/team/TS.png",
+          email: "thomas.sterner@economics.gu.se",
           webpage: "https://sternerthomas.com/thomas-sterner/",
           scholar:
             "https://scholar.google.com/citations?hl=en&user=QPcgMqwAAAAJ",
@@ -83,6 +109,7 @@ export function getStaticProps(): { props: { team: Person[] } } {
         {
           name: "Alexandre Antonelli",
           image: "/images/team/AA.png",
+          email: "alex@captaintech.ai",
           webpage: "https://antonelli-lab.net",
           scholar: "https://scholar.google.se/citations?user=KYbhJxMAAAAJ",
           bio: `Alexandre Antonelli is the Director of Science at the Royal Botanic
@@ -118,8 +145,17 @@ export default function Team({ team }: { team: Person[] }) {
               </Heading>
               <Text mt={2}>
                 <Link
+                  href={`mailto:${person.email}`}
+                  isExternal
+                  opacity={0.5}
+                  _hover={{ opacity: 1 }}
+                >
+                  <EmailIcon size={20} />
+                </Link>
+                <Link
                   href={person.webpage}
                   isExternal
+                  ml={2}
                   title="Silvestro Group"
                   opacity={0.5}
                   _hover={{ opacity: 1 }}
